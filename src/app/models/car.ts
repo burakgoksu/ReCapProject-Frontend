e197215b9377eb418341d5f4e1703d145a4cc8f3
BURAK GÖKSU <gogsub58365@gmail.com>
2021-03-13 18:03:05 +0300

    17.ödev sonu

---
+++ src/app/models/car.ts
@@ -0,0 +1,8 @@
+export interface Car{
+    id:number,
+    modelYear:number,
+    brandName:string,
+    colorName:string,
+    dailyPrice:number,
+    description:string
+}
---
+++ src/app/services/customer.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CustomerResponseModel } from '../models/customerResponseModel';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+  apiUrl ="https://localhost:44320/api/customers/getall";
+  constructor(private httpClient:HttpClient) { }
+
+  getCustomers():Observable<CustomerResponseModel>{
+    return this.httpClient.get<CustomerResponseModel>(this.apiUrl);
+  }
+
+}
